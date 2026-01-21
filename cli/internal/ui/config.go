705280c25d80ab945597592a136185f645a9f6f8
Jimmy Tran <jimmytrandev@gmail.com>
2026-01-21 17:04:33 +0100

    Remove CLI directory and associated files

--- cli/internal/ui/config.go
+++
@@ -1,6 +0,0 @@
-package ui
-
-// GetCurrentTheme returns the current theme instance (always Catppuccin Mocha)
-func GetCurrentTheme() *CatppuccinTheme {
-	return DefaultTheme // DefaultTheme is always Catppuccin Mocha
-}
